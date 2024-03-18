36c394bbd1a06f3e2a86f3a8be36fc4458d11a13
sanskaraggarwal2025 <aggarwalsanskar992@gmail.com>
2024-03-18 22:49:44 +0530

    api eerr fixed

--- frontend/src/Config.ts
+++ frontend/src/Config.ts
@@ -1,3 +1,2 @@
 export const BACKEND_URL = 'https://backend.sanskaraggarwal2025.workers.dev'
 // export const BACKEND_URL = 'http://localhost:8787'
-export const API_KEY = 'o681bcmr2sop7xlkd7vvnfyoc6gfde0tk3xb7vjui2djfw90'
